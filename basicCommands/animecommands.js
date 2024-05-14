01efd86f78e40e890ccf77b00216d369f33411f1
pinksealteam <153603468+pinksealteam@users.noreply.github.com>
2024-05-14 10:21:15 +0200

    Update animecommands.js

--- basicCommands/animecommands.js
+++ basicCommands/animecommands.js
@@ -7,9 +7,9 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setColor('#FFFFFF')
       .setTitle('Anime Commands')
-      .setDescription(`__**✅ List Of Availabe Interations**__\n\n▶️ __**Section 1 :**__\n  blush, cuddle, dance, slap, bonk, bully, hug, confused, kiss, pat, happy, smile.\n\n▶️ __**Section 2 :**__\n yes, highfive, wink, wave, thinking, sad, cry, stare, bored, scream, nervous, kill.\n`)
+      .setDescription(`__**✅ Lista za Tarikov ZestFest Anime Party**__\n\n▶️ __**Section 1 :**__\n  blush, cuddle, dance, slap, bonk, bully, hug, confused, kiss, pat, happy, smile.\n\n▶️ __**Section 2 :**__\n yes, highfive, wink, wave, thinking, sad, cry, stare, bored, scream, nervous, kill.\n`)
       
-      .setImage(`https://cdn.discordapp.com/attachments/1140841446228897932/1142126954775068762/pxfuel.jpg`);
+      .setImage(`https://media.discordapp.net/attachments/1151522152185597952/1239555175870631996/Cool20Text20-20ZestFestTarik20Music20458168465120178.png?ex=664401f7&is=6642b077&hm=833cdd754bff975eef0a8ec43561c729fb7b39260c8da5f1af753540cd2abe8c&=&format=webp&quality=lossless&width=550&height=61`);
     message.reply({ embeds: [embed] });
   },
 };
