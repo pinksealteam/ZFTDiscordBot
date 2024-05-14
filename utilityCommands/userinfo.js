470d41ccf4e4bda05297973d10bbef8e3f7d93c9
pinksealteam <153603468+pinksealteam@users.noreply.github.com>
2024-05-14 10:36:06 +0200

    Update userinfo.js

--- utilityCommands/userinfo.js
+++ utilityCommands/userinfo.js
@@ -14,7 +14,7 @@ module.exports = {
         .setAuthor({
         name: 'User Info!',
         iconURL: 'https://cdn.discordapp.com/attachments/1140841446228897932/1144684108174348318/giphy_1.gif', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        url: 'https://www.youtube.com/channel/UCPOwL09DQXKTVgpMTxQkQZA'
     })
         .setThumbnail(user.avatarURL({ format: 'png', dynamic: true, size: 1024 }))
         .setDescription(`**Username**:  ${user.username}\n**Discriminator**:  ${user.discriminator}\n**ID**:  ${user.id}\n**Joined Discord**:  ${user.createdAt.toUTCString()}`)
@@ -24,24 +24,3 @@ module.exports = {
     },
 };
 
-
-/*
-
-   MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   FOR EMOJIS EITHER YOU CAN EDIT OR JOIN OUR DISCORD SERVER 
-   SO WE ADD BOT TO OUR SERVER SO YOU GET ANIMATED EMOJIS.
-
-   DISCORD SERVER : https://discord.gg/FUEHs7RCqz
-   YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-
-   FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/c4kaW2sSbm ]
-*/
