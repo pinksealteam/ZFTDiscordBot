0a80b61e537a102602d502c9abc4e8a2228a4567
pinksealteam <153603468+pinksealteam@users.noreply.github.com>
2024-05-28 10:34:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.riffy = new Riffy(client, nodes, {
         const guild = client.guilds.cache.get(payload.d.guild_id);
         if (guild) guild.shard.send(payload);
     },
-    defaultSearchPlatform: "ytmsearch",
+    defaultSearchPlatform: "ytsearch",
     restVersion: "v4" 
 });
 
