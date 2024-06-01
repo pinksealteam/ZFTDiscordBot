df0efe8710fc8bd3aaa6c404c35566e739bb9c70
pinksealteam <153603468+pinksealteam@users.noreply.github.com>
2024-06-01 23:05:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,10 @@ const { Classic } = require("musicard");
 const { prefix } = require('./config.json');
 const nodes = [
       {
-            host: "37.114.48.19",
-            password: "youshallnotpass",
-            port: 9601,
-            secure: false
+            host: "lava-v4.ajieblogs.eu.org",
+            password: "https://dsc.gg/ajidevserver",
+            port: 443,
+            secure: true
         },
 ];
 
